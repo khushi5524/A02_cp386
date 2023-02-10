5392e676c2e0ff468f23cb51f7eca87a190731e9
khushi5524 <124849343+khushi5524@users.noreply.github.com>
2023-02-09 22:04:34 -0500

    Update assignment_average.c

--- assignment_average.c
+++ assignment_average.c
@@ -8,28 +8,28 @@
 #define MAX_CHAPTERS 3
 
 
-/*calculate the average grade of each assignment */
-void calculate_average(int chapter, int assignment, int grades[MAX_STUDENTS][MAX_CHAPTERS][MAX_ASSIGNMENTS]) {
+//calculate the average grade of each assignment 
+void cal_average(int chapter, int assignment, int grades[MAX_STUDENTS][MAX_CHAPTERS][MAX_ASSIGNMENTS]) {
     int sum = 0;
     for (int i = 0; i < MAX_STUDENTS; i++) {
-        /* Sum the grades of all students for the current assignment */
+        //Sum the grades of all students for the current assignment
         sum += grades[i][chapter][assignment];
     }
-    /* Calculate the average grade */
+    //Calculate the average grade 
     float average = (float)sum / MAX_STUDENTS;
-    printf("Assignment %d - average = %.6f\n",++assignment, average);
+    printf("Assignment %d - average = %.6f\n", assignment , average);
 }
 
 int main(int argc, char *argv[]) {
     int grades[MAX_STUDENTS][MAX_CHAPTERS][MAX_ASSIGNMENTS];
     FILE *fp;
     fp = fopen("sample_in_grades.txt", "r");
-    /* To check if the file was successfully opened */
+    //To check if the file was successfully opened
     if (fp == NULL) {
         printf("Error opening file\n");
         exit(1);
     }
-    /* Check grades from the file into the grades array */
+    //Check grades from the file into the grades array
     for (int i = 0; i < MAX_STUDENTS; i++) {
         for (int j = 0; j < MAX_CHAPTERS; j++) {
             for (int k = 0; k < MAX_ASSIGNMENTS; k++) {
@@ -37,30 +37,30 @@ int main(int argc, char *argv[]) {
             }
         }
     }
-    /* flies closes here */
+    //flies closes here
     fclose(fp);
 
     pid_t pid;
-    /* Loop of all chapters */
+    //Loop of all chapters
     for (int chapter = 0; chapter < MAX_CHAPTERS; chapter++) {
-        /* Generate a GradTA process for each chapter */
+        //Generate a GradTA process for each chapter
         pid = fork();
 
         if (pid == 0) {
             for (int assignment = 0; assignment < MAX_ASSIGNMENTS; assignment++) {
                 pid = fork();
                 if (pid == 0) {
-                    /* Calculate and print the average grade for the current assignment */
-                    calculate_average(chapter, assignment, grades);
-                    /* Exit the TA process */
+                    // Calculate and print the average grade for the current assignment 
+                    cal_average(chapter, assignment, grades);
+                    //Exit the TA process 
                     exit(0);
                 }
-                /* Wait for the current TA process to complete */
+                //Wait for the current TA process to complete 
                 wait(NULL);
             }
             exit(0);
         }
-        /* Wait for the current GradTA process to complete */
+        //Wait for the current GradTA process to complete 
         wait(NULL);
     }
 
